abc8c13aac432a311043d4ac8a5a20f72a17564b
Raushan <raushanraj@gmail.com>
2020-05-18 00:28:53 +0530

    module1-solution

--- myctrl.js
+++ myctrl.js
@@ -1,20 +1,34 @@
 app.controller("myctrl",function($scope)
 {
- $scope.name = "";
+ $scope.name = [];
+ // $scope.greet="";
+ // $scope.over="";
+ // $scope.empty="";
  $scope.totalvalue= 0;
  $scope.displaynumeric=function()
  {
    var totalnamevalue=calculatornumericstring($scope.name);
    $scope.totalvalue=totalnamevalue;
+ return  $scope.totalvalue;
  };
-
+ $scope.check=function()
+ {
+   $scope.cake="hi";
+   return $scope.cake;
+ }
+$scope.changer=function()
+{
+  $scope.greet="Enjoy!";
+  $scope.over="Too much!";
+   $scope.empty="Please enter data first";
+}
  function calculatornumericstring(string)
  {
-   var totalstringvalue=0;
-   for(var i=0;i<string.length;i++)
-   {
-     totalstringvalue+=string.charCodeAt(i);
-   }
+   var totalstringvalue=$scope.name.length;
+   // for(var i=0;i<string.length;i++)
+   // {
+   //   totalstringvalue+=string.charCodeAt(i);
+   // }
    return totalstringvalue;
  }
 
